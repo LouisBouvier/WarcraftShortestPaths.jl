d3f1ee6715cd33ed9a108c51f66bf8baa2d73696
Documenter.jl <documenter@juliadocs.github.io>
2022-05-23 09:46:16 +0000

    build based on 9075655

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WarcraftShortestPath","category":"page"},{"location":"#WarcraftShortestPath","page":"Home","title":"WarcraftShortestPath","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WarcraftShortestPath.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WarcraftShortestPath]","category":"page"}]
+}
